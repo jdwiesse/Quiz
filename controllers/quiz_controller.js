14a1694d6dc69e0d58faa011112db2206b11b755
Javier wiesse <jdwiesse_5@hotmail.com>
2015-06-17 16:47:37 -0500

    proyecto final

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,18 @@
+//get /quizes/question
+exports.question = function(req,res) {
+   res.render('quizes/question' ,{pregunta: 'Capital de italia'});
+};
+
+//get /quizes/answer
+exports.answer = function(req,res) {
+	if(req.query.respuesta ==='Roma'){
+		res.render('quizes/answer' ,{respuesta: 'Correcto'});
+} else {res.render('quizes/answer' ,{respuesta: 'Incorrecto'});
+
+}
+};
+
+// get /author
+exports.author = function(req,res) {
+	res.render('author',{autor: 'Javier Wiesse',foto : '/images/foto.jpg',video: '/images/ContaminaciondelAgua.mp4'} );
+};
